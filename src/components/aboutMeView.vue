d7994e3ba6b97c614baeccbfb8b18543d932b932
mangosteen977 <mosubin977@daum.net>
2024-02-18 23:05:43 +0900

    edited aboutMe view page

--- src/components/aboutMeView.vue
+++ src/components/aboutMeView.vue
@@ -62,56 +62,6 @@
             </label>
           </div>
         </div>
-        <!-- <div class="aboutMe-timeLine-graph">
-          <div class="aboutMe-timeLine-graph-event">
-            <div class="aboutMe-timeLine-graph-dot">
-              <label>2019.02</label>
-            </div>
-            <span class="aboutMe-timeLine-graph-visible">
-              <div class="aboutMe-timeLine-graph-line"></div>
-              <label class="aboutMe-timeLine-graph-event-text"
-                >청강문화산업대학교 식품영양과 졸업<br />
-                영양사, 위생사 면허 취득
-              </label>
-            </span>
-          </div>
-          <div class="aboutMe-timeLine-graph-event">
-            <div class="aboutMe-timeLine-graph-dot">
-              <label>2019.09~2020.03</label>
-            </div>
-            <span class="aboutMe-timeLine-graph-visible">
-              <div class="aboutMe-timeLine-graph-line"></div>
-              <label class="aboutMe-timeLine-graph-event-text"
-                >베트남 호치민 인문사회과학대학교 어학당<br />
-                베트남어 VSL1 ~ VSL3 과정 수강
-              </label>
-            </span>
-          </div>
-          <div class="aboutMe-timeLine-graph-event">
-            <div class="aboutMe-timeLine-graph-dot">
-              <label>2021.02~2021.08 </label>
-            </div>
-            <span class="aboutMe-timeLine-graph-visible">
-              <div class="aboutMe-timeLine-graph-line"></div>
-              <label class="aboutMe-timeLine-graph-event-text"
-                >종로 그린컴퓨터아트학원<br />
-                웹퍼블리셔 프론트엔드 개발 디지털실무 양성과정 수강
-              </label>
-            </span>
-          </div>
-          <div class="aboutMe-timeLine-graph-event">
-            <div class="aboutMe-timeLine-graph-dot">
-              <label>2021.10~2023.10 </label>
-            </div>
-            <span class="aboutMe-timeLine-graph-visible">
-              <div class="aboutMe-timeLine-graph-line"></div>
-              <label class="aboutMe-timeLine-graph-event-text"
-                >Telus International AI Korea 2년 재직<br />
-                인공지능 서비스 유지 보수 및 기능 개발 프로젝트 참여
-              </label>
-            </span>
-          </div>
-        </div> -->
       </li>
       <li>
         <h2>| Skills</h2>
@@ -119,54 +69,57 @@
           <!-- Frontend -->
           <h4>Frontend</h4>
           <div class="aboutMe-skills-list">
-            <img
-              alt="html"
-              src="https://img.shields.io/badge/html5-E34F26?style=flat-square&logo=html5&logoColor=white"
-            />
-            <img
-              alt="css3"
-              src="https://img.shields.io/badge/css-1572B6?style=flat-square&logo=css3&logoColor=white"
-            />
-            <img
-              alt="Sass"
-              src="https://img.shields.io/badge/Sass-CC6699?style=flat-square&logo=Sass&logoColor=white"
-            />
-            <img
-              alt="javascript"
-              src="https://img.shields.io/badge/javascript-F7DF1E?style=flat-square&logo=javascript&logoColor=black"
-            />
-            <img
-              alt="jquery"
-              src="https://img.shields.io/badge/jquery-0769AD?style=flat-square&logo=jquery&logoColor=white"
-            />
-            <img
-              alt="vue.js"
-              src="https://img.shields.io/badge/vue.js-4FC08D?style=flat-square&logo=vuedotjs&logoColor=white"
-            />
-            <img
-              alt="bootstrap"
-              src="https://img.shields.io/badge/bootstrap-7952B3?style=flat-square&logo=bootstrap&logoColor=white"
-            />
+            <div class="aboutMe-skills-badge badge-html" title="html">
+              <span class="aboutMe-skills-badge-svg"></span>
+              html
+            </div>
+            <div class="aboutMe-skills-badge badge-css" title="css">
+              <span class="aboutMe-skills-badge-svg"></span>
+              css
+            </div>
+            <div class="aboutMe-skills-badge badge-scss" title="scss">
+              <span class="aboutMe-skills-badge-svg"></span>
+              scss
+            </div>
+            <div
+              class="aboutMe-skills-badge badge-javascript"
+              title="javascript"
+            >
+              <span class="aboutMe-skills-badge-svg"></span>
+              javascript
+            </div>
+            <div class="aboutMe-skills-badge badge-jquery" title="jquery">
+              <span class="aboutMe-skills-badge-svg"></span>
+              jquery
+            </div>
+            <div class="aboutMe-skills-badge badge-vue" title="vue">
+              <span class="aboutMe-skills-badge-svg"></span>
+              vue
+            </div>
+            <div class="aboutMe-skills-badge badge-bootstrap" title="bootstrap">
+              <span class="aboutMe-skills-badge-svg"></span>
+              bootstrap
+            </div>
           </div>
           <!-- Tools -->
           <h4>Tools</h4>
           <div class="aboutMe-skills-list">
-            <img
-              alt="git"
-              src="https://img.shields.io/badge/git-F05032?style=flat-square&logo=git&logoColor=white"
-            />
-            <img
-              alt="github"
-              src="https://img.shields.io/badge/github-181717?style=flat-square&logo=github&logoColor=white"
-            />
-            <img
-              alt="slack"
-              src="https://img.shields.io/badge/slack-4A154B?style=flat-square&logo=slack&logoColor=white"
-            />
-            <img
-              alt="atlassian"
-              src="https://img.shields.io/badge/atlassian-0052CC?style=flat-square&logo=atlassian&logoColor=white"
-            />
+            <div class="aboutMe-skills-badge badge-git" title="git">
+              <span class="aboutMe-skills-badge-svg"></span>
+              git
+            </div>
+            <div class="aboutMe-skills-badge badge-github" title="github">
+              <span class="aboutMe-skills-badge-svg"></span>
+              github
+            </div>
+            <div class="aboutMe-skills-badge badge-slack" title="slack">
+              <span class="aboutMe-skills-badge-svg"></span>
+              slack
+            </div>
+            <div class="aboutMe-skills-badge badge-atlassian" title="atlassian">
+              <span class="aboutMe-skills-badge-svg"></span>
+              atlassian
+            </div>
           </div>
         </div>
       </li>
@@ -244,90 +197,7 @@ export default {
 .aboutMe-timeLine > div > label {
   padding-left: 30px;
 }
-/*타임라인 그래프*/
-.aboutMe-timeLine-graph {
-  /*border: 1px solid hotpink;*/
-  width: 100%;
-  height: auto;
-  padding: 10px;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: baseline;
-}
-.aboutMe-timeLine-graph-event {
-  /*border: 1px solid hotpink;*/
-  border-top: 3px solid black;
-  width: calc(100% / 4 - 20px);
-  height: fit-content;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: first baseline;
-  flex-wrap: nowrap;
-}
-.aboutMe-timeLine-graph-dot {
-  width: 10px;
-  height: 10px;
-  background-color: black;
-  border-radius: 100%;
-  position: relative;
-  z-index: 100;
-  top: -5px;
-}
-.aboutMe-timeLine-graph-dot > label {
-  width: max-content;
-  height: 20px;
-  position: absolute;
-  top: -15px;
-  font-size: 12px;
-  font-weight: 400;
-}
-.aboutMe-timeLine-graph-line {
-  width: 100px;
-  height: 2px;
-  background-color: #ffc000;
-  transform: rotate(15deg);
-  margin-top: 3px;
-}
-.aboutMe-timeLine-graph-event-text {
-  width: max-content;
-  height: fit-content;
-  border: 2px solid #ffc000;
-  background-color: #ffc000;
-  position: relative;
-  z-index: 100;
-  margin-left: 50px;
-  font-size: 13px;
-  font-weight: 400;
-}
 
-/*.aboutMe-timeLine-graph-event 마지막*/
-.aboutMe-timeLine-graph
-  > .aboutMe-timeLine-graph-event:last-child
-  > .aboutMe-timeLine-graph-visible
-  > .aboutMe-timeLine-graph-line {
-  transform: rotate(-15deg);
-  position: relative;
-  left: -100px;
-}
-.aboutMe-timeLine-graph
-  > .aboutMe-timeLine-graph-event:last-child
-  > .aboutMe-timeLine-graph-visible
-  > .aboutMe-timeLine-graph-event-text {
-  left: calc(-100% + -50px);
-}
-
-/*.aboutMe-timeLine-graph-dot:hover 타임라인 점 호버*/
-.aboutMe-timeLine-graph-visible {
-  visibility: hidden;
-}
-.aboutMe-timeLine-graph-dot:hover + .aboutMe-timeLine-graph-visible {
-  visibility: visible;
-}
-.aboutMe-timeLine-graph-dot:hover {
-  background-color: #ffc000;
-}
 /*skills*/
 .aboutMe-skills {
   padding: 0px 50px;
@@ -339,7 +209,7 @@ export default {
   align-items: center;
   justify-content: flex-start;
   flex-wrap: wrap;
-  gap: 5px;
+  gap: 10px;
   padding-left: 10px;
 }
 .aboutMe-skills-list > img {
@@ -348,6 +218,102 @@ export default {
   object-fit: contain;
   margin-right: 5px;
   /*border: 1px solid hotpink;*/
+}
+
+/*skills badge*/
+.aboutMe-skills-badge {
+  width: fit-content;
+  height: 25px;
+  padding: 7px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 7px;
+  font-size: 14px;
+  color: rgb(255, 255, 255);
+}
+.aboutMe-skills-badge-svg {
+  width: 18px;
+  height: 18px;
+}
+/*badge list*/
+/*Javascript*/
+.badge-javascript {
+  background-color: #f7df1e;
+  color: #000000;
+}
+.badge-javascript > .aboutMe-skills-badge-svg {
+  background-image: url("@/assets/svg/javascript.svg");
+}
+/*html*/
+.badge-html {
+  background-color: #e34f26;
+}
+.badge-html > .aboutMe-skills-badge-svg {
+  background-image: url("@/assets/svg/html.svg");
+}
+/*css*/
+.badge-css {
+  background-color: #1572b6;
+}
+.badge-css > .aboutMe-skills-badge-svg {
+  background-image: url("@/assets/svg/css.svg");
+}
+/*scss*/
+.badge-scss {
+  background-color: #cc6699;
+}
+.badge-scss > .aboutMe-skills-badge-svg {
+  background-image: url("@/assets/svg/scss.svg");
+}
+/*jquery*/
+.badge-jquery {
+  background-color: #0769ad;
+}
+.badge-jquery > .aboutMe-skills-badge-svg {
+  background-image: url("@/assets/svg/jquery.svg");
+}
+/*vue*/
+.badge-vue {
+  background-color: #4fc08d;
+}
+.badge-vue > .aboutMe-skills-badge-svg {
+  background-image: url("@/assets/svg/vue.svg");
+}
+/*bootstrap*/
+.badge-bootstrap {
+  background-color: #7952b3;
+}
+.badge-bootstrap > .aboutMe-skills-badge-svg {
+  background-image: url("@/assets/svg/bootstrap.svg");
+}
+/*git*/
+.badge-git {
+  background-color: #f05032;
+}
+.badge-git > .aboutMe-skills-badge-svg {
+  background-image: url("@/assets/svg/git.svg");
+}
+/*github*/
+.badge-github {
+  background-color: #181717;
+}
+.badge-github > .aboutMe-skills-badge-svg {
+  background-image: url("@/assets/svg/github.svg");
+}
+/*slack*/
+.badge-slack {
+  background-color: #4a154b;
+}
+.badge-slack > .aboutMe-skills-badge-svg {
+  background-image: url("@/assets/svg/slack.svg");
+}
+/*atlassian*/
+.badge-atlassian {
+  background-color: #0052cc;
+}
+.badge-atlassian > .aboutMe-skills-badge-svg {
+  background-image: url("@/assets/svg/atlassian.svg");
 }
 
 @media (min-width: 480px) and (max-width: 600px) {
