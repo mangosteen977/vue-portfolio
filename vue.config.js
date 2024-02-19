65a2dfe2e5248fa69c80ebbba894cfb458343ae9
mangosteen977 <mosubin977@daum.net>
2024-02-20 00:07:38 +0900

    edited config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 const path = require('path')
 
 module.exports = defineConfig({
-  publicPath: 'vue-portfolio/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-portfolio/' : '/',
   transpileDependencies: true,
   // chainWebpack: config => {
   //   config.resolve.alias
